e376fa98a6972490f0295011ff880df2d5c51abc
zunbeeok <zunbeeok@gmail.com>
2024-04-03 20:40:55 +0900

    product-list 거래 가능만 보기 기능 추가

--- src/pages/product-list/index.js
+++ src/pages/product-list/index.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import Nav from "../../components/molecules/nav";
 import styles from "./ProductList.module.scss";
 import { useLocation } from "react-router-dom";
-import { apiClient } from "../../api/client";
 import { getProducts } from "../../api/camp-daddy";
 import { handleImgError } from "../../components/handleImage";
 
 export default function ProductList() {
   const location = useLocation();
   const [products, setProducts] = useState([]);
+  const [showAvailableOnly, setShowAvailableOnly] = useState(false);
 
   useEffect(() => {
     const searchParam = new URLSearchParams(location.search)
@@ -18,28 +18,42 @@ export default function ProductList() {
       endDate: searchParam.get("endDate"),
       category: searchParam.get("category"),
       page: searchParam.get("page") - 1,
-      filterReservation: searchParam.get("filterReservation")
+      filterReservation: searchParam.get("filterReservation") === "true" // Convert to boolean
     }
+    setShowAvailableOnly(searchData.filterReservation);
 
     const fetchData = async () => {
       try {
         const data = await getProducts(searchData);
+        data.content.forEach(e => {
+          e.image = !e.image ? "" : e.image
+        })
         setProducts(data.content);
       } catch (error) {
         console.error("Error fetching products:", error);
       }
     };
 
-
     fetchData();
   }, [location.search])
 
+  const handleCheckboxChange = () => {
+    setShowAvailableOnly(!showAvailableOnly);
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set("filterReservation", !showAvailableOnly ? "true" : "false");
+    window.location.href = `${location.pathname}?${searchParams.toString()}`;
+  };
 
   return (
     <div className={styles.my_main}>
       <h2 className={styles.title}>상품리스트</h2>
       <div className={styles.checkbox}>
-        <input type="checkbox" id="checkbox01" />
+        <input
+          type="checkbox"
+          id="checkbox01"
+          checked={showAvailableOnly}
+          onChange={handleCheckboxChange} // Handle checkbox change event
+        />
         <label htmlFor="checkbox01">거래 가능만 보기</label>
       </div>
       <div className={styles.product_wrap}>
@@ -58,6 +72,6 @@ export default function ProductList() {
         ))}
       </div>
       <Nav />
-    </div>
+    </div >
   );
 }
