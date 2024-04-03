f1eec6185a1421d33d940b0273b605c05b99c4dc
seunghwan <lovelyunsh@naver.com>
2024-04-03 18:00:48 +0900

    feat : nav바 독립 시키기

--- src/components/molecules/nav.js
+++ src/components/molecules/nav.js
@@ -6,58 +6,59 @@ import { IoMdAddCircleOutline } from "react-icons/io";
 import { AiFillHome } from "react-icons/ai";
 
 export default function Nav() {
-    // 로그인 여부
-    const user = !localStorage.getItem("access_token");
-    return (
-        <div className={styles.nav}>
-            <button
-                onClick={() => {
-                    if (user) {
-                        window.location.href = "/sign-in";
-                    } else {
-                        window.location.href = "/product-add";
-                    }
-                }
-                }
-            >
-                <IoMdAddCircleOutline size={45}/>
-            </button>
-            <button
-                onClick={() => {
-                    window.location.href = "/search";
-                }}
-            >
-                <FiSearch size={45}/>
-            </button>
-            <button
-                onClick={() => {
-                    window.location.href = "/";
-                }}
-            >
-                <AiFillHome size={45}/>
-            </button>
-            <button
-                onClick={() => {
-                    if (user) {
-                        window.location.href = "/sign-in";
-                    } else {
-                        window.location.href = "/chat-list";
-                    }
-                }}
-            >
-                <IoChatboxOutline size={45}/>
-            </button>
-            <button
-                onClick={() => {
-                    if (user) {
-                        window.location.href = "/sign-in";
-                    } else {
-                        window.location.href = "/my-page";
-                    }
-                }}
-            >
-                <AiOutlineUser size={45}/>
-            </button>
-        </div>
-    );
+  // 로그인 여부
+  const user = !localStorage.getItem("access_token");
+  return (
+    <div className={styles.navdiv}>
+      <div className={styles.nav}>
+        <button
+          onClick={() => {
+            if (user) {
+              window.location.href = "/sign-in";
+            } else {
+              window.location.href = "/product-add";
+            }
+          }}
+        >
+          <IoMdAddCircleOutline size={45} />
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = "/search";
+          }}
+        >
+          <FiSearch size={45} />
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          <AiFillHome size={45} />
+        </button>
+        <button
+          onClick={() => {
+            if (user) {
+              window.location.href = "/sign-in";
+            } else {
+              window.location.href = "/chat-list";
+            }
+          }}
+        >
+          <IoChatboxOutline size={45} />
+        </button>
+        <button
+          onClick={() => {
+            if (user) {
+              window.location.href = "/sign-in";
+            } else {
+              window.location.href = "/my-page";
+            }
+          }}
+        >
+          <AiOutlineUser size={45} />
+        </button>
+      </div>
+    </div>
+  );
 }
